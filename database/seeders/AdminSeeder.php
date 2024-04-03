203689d17faf53d66f75b9ec50bf55b54b16142a
salehuddn <muhammadsalehuddin1@gmail.com>
2024-04-04 00:46:42 +0800

    feat: add admin seeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $admin = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@mail.com',
+            'password' => Hash::make('password'),
+            'phone' => '08123456789',
+            'dob' => '1990-01-01'
+        ]);
+
+        $admin->assignRole('admin');
+    }
+}
