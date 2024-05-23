a8ab8280ffd94abb49adfa787cbad077531681ea
salehuddn <muhammadsalehuddin1@gmail.com>
2024-05-24 01:40:57 +0800

    feat: update notification check spending limit

--- app/Console/Commands/CheckDependantSpendingLimit.php
+++ app/Console/Commands/CheckDependantSpendingLimit.php
@@ -33,8 +33,7 @@ class CheckDependantSpendingLimit extends Command
         foreach ($users as $user) {
             $result = SpendingService::hasAlmostExceededLimit($user);
             if ($result && $result['code'] === '200') {
-                // determine the title based on user type
-                $title = $user->hasRole('guardian') ? "User: {$user->name}" : 'Spending Limit Alert';
+                $title = 'Spending Limit Alert';
                 
                 // notify the user with the appropriate title
                 $user->notify(new SpendingLimitNotification($title, $result['message']));
@@ -42,8 +41,10 @@ class CheckDependantSpendingLimit extends Command
                 // notify the guardian if it's a dependant
                 if ($user->hasRole('dependant')) {
                     $guardian = $user->guardians()->first();
+                    $title = "User: {$user->name}";
+                    
                     if ($guardian) {
-                        $guardian->notify(new SpendingLimitNotification("Your dependant {$user->name} has spent more than 70% of their spending limit"));
+                        $guardian->notify(new SpendingLimitNotification($title, "Your dependant {$user->name} has spent more than 70% of their spending limit"));
                     }
                 }
             }
