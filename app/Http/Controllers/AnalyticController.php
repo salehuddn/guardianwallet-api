c611f13171716a0bd937eedfd94d84e561a1997d
salehuddn <muhammadsalehuddin1@gmail.com>
2024-06-26 23:25:02 +0800

    feat: import missin txn service

--- app/Http/Controllers/AnalyticController.php
+++ app/Http/Controllers/AnalyticController.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use App\Models\UserTransaction;
 use App\Services\AnalyticService;
+use App\Services\TransactionService;
 
 class AnalyticController extends Controller
 {
